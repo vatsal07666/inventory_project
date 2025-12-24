e0c9d69b35578ba8da4125c50027db56b443180e
V S <vsdeveloper@gmail.com>
2025-12-24 13:38:43 +0530

    change path

--- src/files/login/loginpage.jsx
+++ src/files/login/loginpage.jsx
@@ -1,56 +1,8 @@
-// import { Formik, Form, Field } from "formik";
-// import * as Yup from "yup";
-// import { Link } from "react-router-dom";
-
-// const LoginSchema = Yup.object({
-//     username: Yup.string().required("Username is required*"),
-//     password: Yup.string().required("Password is required*"),
-// });
-
-// const Login = () => {
-//     return (
-//         <div className="login-container">
-//             <Formik initialValues={{ username: "", password: "" }}
-//                     validationSchema={LoginSchema}
-//                     onSubmit={(values, { resetForm }) => {
-//                     console.log("Login Data:", values);
-//                     resetForm();
-//                 }} >
-//                 {({ errors, touched }) => (
-//                     <Form className="login-box">
-//                         <h2>Sign in</h2>
-
-//                         <label>Username</label>
-//                         <Field name="username" type="text" placeholder="Enter Username" />
-//                         {errors.username && touched.username && (<p style={{color:"red"}}>{errors.username}</p>)}
-//                         <br />
-
-//                         <label>Password</label>
-//                         <Field name="password" type="password" placeholder="Enter Password" />
-//                         {errors.password && touched.password && (<p style={{color:"red"}}>{errors.password}</p>)}
-//                         <br />
-
-//                         <button type="submit">Log In</button>
-
-//                         <p className="signup-text">
-//                             Don’t have an account?
-//                             <Link to="/register" className="signup-button"> Sign Up </Link>
-//                         </p>
-//                     </Form>
-//                 )}
-//             </Formik>
-//         </div>
-//     );
-// };
-
-// export default Login;
-
-
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { Link, useHistory } from "react-router-dom";
-import { Snackbar, Alert, Button } from "@mui/material";
+import { Snackbar, Alert, Button, Box } from "@mui/material";
 
 const LoginSchema = Yup.object({
     username: Yup.string().required("Username is required*"),
@@ -61,10 +13,19 @@ const Login = () => {
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const history = useHistory();
 
-    return (
-        <div className="login-container">
+    const formikRef = useRef(null);
 
-            <Formik
+    const fillDemoData = () => {
+        formikRef.current.setValues({
+            username: "demo@gmail.com",
+            password: "demo123",
+        });
+    };
+
+    return (
+        <Box className="login-container">
+
+            <Formik innerRef={formikRef}
                 initialValues={{ username: "", password: "" }}
                 validationSchema={LoginSchema}
                 onSubmit={(values, { resetForm }) => {
@@ -72,7 +33,7 @@ const Login = () => {
                     setOpenSnackbar(true);
                     resetForm();
 
-                    history.push("/");
+                    history.push("/dashboard");
                 }}
             >
                 {({ errors, touched }) => (
@@ -103,6 +64,10 @@ const Login = () => {
                 )}
             </Formik>
 
+            <Button variant="contained" onClick={fillDemoData} sx={{mt: 3}}>
+                username: demo@gmail.com, password: demo123
+            </Button>
+
             {/* Snackbar */}
             <Snackbar
                 open={openSnackbar}
@@ -115,7 +80,7 @@ const Login = () => {
                 </Alert>
             </Snackbar>
 
-        </div>
+        </Box>
     );
 };
 
