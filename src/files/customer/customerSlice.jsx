e4db34b1ed7fa1d691c5128b3272a91d424665e3
V S <vsdeveloper@gmail.com>
2026-01-06 13:57:20 +0530

    api slice file

--- src/files/customer/customerSlice.jsx
+++ src/files/customer/customerSlice.jsx
@@ -3,54 +3,70 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     list: [],
     open: false,
-    editIndex: null,
     deleteOpen: false,
-    deleteIndex: null
-}
+    deleteId: null,
+    editId: null,
+};
 
 const customerSlice = createSlice({
     name: "customer",
     initialState,
     reducers: {
+        // ================= UI STATE =================
         setOpen: (state, action) => {
             state.open = action.payload;
         },
-        setEditIndex: (state, action) => {
-            state.editIndex = action.payload;
+        setEditId: (state, action) => {
+            state.editId = action.payload;
         },
         resetUIState: (state) => {
             state.open = false;
-            state.editIndex = null;
+            state.editId = null;
         },
 
         setDeleteOpen: (state, action) => {
             state.deleteOpen = action.payload;
         },
-        setDeleteIndex: (state, action) => {
-            state.deleteIndex = action.payload;
+        setDeleteId: (state, action) => {
+            state.deleteId = action.payload;
         },
         resetDeleteState: (state) => {
             state.deleteOpen = false;
-            state.deleteIndex = null; 
+            state.deleteId = null;
         },
 
+        // ================= DATA STATE =================
+
+        // GET
+        setCustomer: (state, action) => {
+            state.list = action.payload;
+            localStorage.setItem("customers", JSON.stringify(state.list));
+        },
+
+        // POST
         addCustomer: (state, action) => {
             state.list.push(action.payload);
             localStorage.setItem("customers", JSON.stringify(state.list));
         },
-        updateCustomer: (state, action) => {
-            const { index, customer } = action.payload;
-            state.list[index] = customer;
+
+        // DELETE
+        deleteCustomer: (state, action) => {
+            state.list = state.list.filter((item) => item._id !== action.payload);
             localStorage.setItem("customers", JSON.stringify(state.list));
         },
-        deleteCustomer: (state, action) => {
-            state.list.splice(action.payload, 1);
-            localStorage.setItem("customers", JSON.stringify(state.list));
-        }
-    }
-})
 
-export const { setOpen, setEditIndex, resetUIState, setDeleteOpen, setDeleteIndex, resetDeleteState, addCustomer,
-    updateCustomer, deleteCustomer
- } = customerSlice.actions;
+        // Edit / PATCH
+        updateCustomer: (state, action) => {
+            const index = state.list.findIndex((item) => item._id === action.payload._id );
+            // findIndex return -1 if "not found"(the data)
+            if(index !== -1) state.list[index] = action.payload;
+            localStorage.setItem("customers", JSON.stringify(state.list));
+        },
+    },
+});
+
+export const { setOpen, setEditId, resetUIState, setDeleteOpen, setDeleteId, resetDeleteState,
+    setCustomer, addCustomer, deleteCustomer, updateCustomer
+} = customerSlice.actions;
+
 export default customerSlice.reducer;
